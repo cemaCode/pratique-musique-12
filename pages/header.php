72d69b85b4364c425ffe10734748ce9c3bbd82c8
lisalefrancois <lisalefrancois.perso@gmail.com>
2022-10-07 10:30:58 +0200

    Signed-off-by: lisalefrancois <lisalefrancois.perso@gmail.com>

---
+++ pages/header.php
@@ -0,0 +1,25 @@
+<button name="connexion"><a href="/pratique-musique-12/pages/connexion.php">Connexion</a></button>
+
+<header>
+    <h1><a href="/pratique-musique-12/index.php">pratique-musique-12</a></h1>
+    <nav>
+        <ul>
+            <li><a href="/pratique-musique-12/index.php">Accueil</a></li>
+            <li class="deroulant"><a href="/pratique-musique-12/pages/rubriques.php">Rubriques</a>
+                <ul class="sous">
+                    <li><a href="/pratique-musique-12/pages/rubriques.php">Rubriques</a></li>
+                    <li><a href="/pratique-musique-12/pages/rubriques/eveil_musical.php">Eveil musical</a></li>
+                    <li><a href="/pratique-musique-12/pages/rubriques/enseignements.php">Enseignements</a></li>
+                    <li><a href="/pratique-musique-12/pages/rubriques/pratique_ensemble.php">Pratiques d'ensemble</a></li>
+                    <li><a href="/pratique-musique-12/pages/rubriques/accompagnement.php">Accompagnement</a></li>
+                    <li><a href="/pratique-musique-12/pages/rubriques/diffusion.php">Diffusion</a></li>
+                </ul>
+            </li>
+            <li><a href="/pratique-musique-12/pages/recherche.php">Recherche</a></li>
+            <li><a href="/pratique-musique-12/pages/contact.php">Contact</a>
+        </ul>
+    </nav>
+</header>
+
+
+
