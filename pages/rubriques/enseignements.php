d88d7ec7b21c35ebddee61e301cbeb80fed60fcd
c.el-molghy.20 <c.el-molghy@formation-cci12.fr>
2022-10-19 10:16:09 +0200

    ajout getOffresByRubrique

--- pages/rubriques/enseignements.php
+++ pages/rubriques/enseignements.php
@@ -13,30 +13,30 @@ include('../head.php')
         require_once($_SERVER['DOCUMENT_ROOT'] . "/pratique-musique-12/testing/dbController.php");
 
         $db = new dbController();
-        if(!isset($_GET['nomInstrument'])){
-        $instruments = $db->getInstruments();
-        echo "<h2><ul>";
-        foreach ($instruments as $key =>   $value) {
-            $nomInstrument = $value['nomInstrument'];
-            $url  = $_SERVER['REQUEST_URI'] . "?nomInstrument=" . $nomInstrument;
-            echo "<li><a href=" . $url . "  >" . $nomInstrument . "</a></li>";
+        if (!isset($_GET['nomInstrument'])) {
+            $instruments = $db->getInstruments();
+            echo "<h2><ul>";
+            foreach ($instruments as $key =>   $value) {
+                $nomInstrument = $value['nomInstrument'];
+                $url  = $_SERVER['REQUEST_URI'] . "?nomInstrument=" . $nomInstrument;
+                echo "<li><a href=" . $url . "  >" . $nomInstrument . "</a></li>";
+            }
+            echo "</ul></h2>";
+        } else {
+            $nomInstrument = $_GET['nomInstrument'];
+            $structures = $db->getStructureByInstrument($nomInstrument);
+            $GLOBALS['structures'] = $structures;
+            echo "<h2>Voici les structures proposant l'instrument : {$nomInstrument}</h2>";
+            include("../affichageStructures.php");
+
+            // ICI ON POURRAIT AVOIR UNE PAGE PHP QUI PREND LA LISTE DES STRUCTURES PEU IMPORTE LA RUBRIQUE
+            // CETTE PAGE POURRAIT ETRE UTILISEE POUR TOUTES LES RUBRIQUES AVEC UN STYLE EN CARDS
+            // CF ARTSVIVANTS52.ORG/BASE-DE-DONNEES.HTML
+
         }
-        echo "</ul></h2>";
 
-    }else{
-        $nomInstrument = $_GET['nomInstrument'];
-        $structures = $db->getStructureByInstrument();
-
-        echo "Voici les structures proposant l'instrument : ". $nomInstrument;
-
-        // ICI ON POURRAIT AVOIR UNE PAGE PHP QUI PREND LA LISTE DES STRUCTURES PEU IMPORTE LA RUBRIQUE
-        // CETTE PAGE POURRAIT ETRE UTILISEE POUR TOUTES LES RUBRIQUES AVEC UN STYLE EN CARDS
-        // CF ARTSVIVANTS52.ORG/BASE-DE-DONNEES.HTML
-
-    }
-
-    ?>
-</section>
+        ?>
+    </section>
 
     <?php
     include('../footer.php');
--- testing/dbController.php
+++ testing/dbController.php
@@ -1,10 +1,11 @@
 <?php
 
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+	session_start();
 }
-class dbController{
-	
+class dbController
+{
+
 	private $_server = 'localhost'; // Nom serveur
 	private	$_user = 'root'; // User MySQL 
 	private	$_password = ''; // MdP MySQL
@@ -13,98 +14,115 @@ class dbController{
 
 
 	// Constructeur 
-	public function __construct(){ 
+	public function __construct()
+	{
 		//Connexion à la base de données 
-		$this->_mysqli = new mysqli($this->_server,$this->_user,$this->_password,$this->_database);
+		$this->_mysqli = new mysqli($this->_server, $this->_user, $this->_password, $this->_database);
 
 		// Check connection
-		if ($this->_mysqli -> connect_errno) {
-				echo 'Erreur de connection /!\ : '.$this->_mysqli -> error;
-				exit();
+		if ($this->_mysqli->connect_errno) {
+			echo 'Erreur de connection /!\ : ' . $this->_mysqli->error;
+			exit();
 		}
 	}
 
 	// Ajouter un instrument dans la table 'Intruments"
 	public function addInstrument($nomInstrument)
 	{
-		$req = "INSERT INTO `instruments` (`nomInstrument`) VALUES ('" . $nomInstrument ."')";
-		if ($result = $this->_mysqli -> query($req)) {
-			echo "Instrument: ". $nomInstrument ." ajouté avc succès." ;
-		  } else {
+		$req = "INSERT INTO `instruments` (`nomInstrument`) VALUES ('" . $nomInstrument . "')";
+		if ($result = $this->_mysqli->query($req)) {
+			echo "Instrument: " . $nomInstrument . " ajouté avc succès.";
+		} else {
 			echo "Error: impossible d'ajotuer l'instrument";
-		  }
+		}
 	}
 
 	// Récupèrer une colonne de la table Rubriques
-	public function getRubriques(){
+	public function getRubriques()
+	{
 		$req = 'SELECT nomRubrique FROM rubriques';
-		if ($result = $this->_mysqli -> query($req)) {
+		if ($result = $this->_mysqli->query($req)) {
 			$dump = $result->fetch_all(MYSQLI_ASSOC);
-			  $result -> free_result();
-			}
-			return $dump;
+			$result->free_result();
+		}
+		return $dump;
 	}
 
 	// Réupère tous les instruments en ordre alphabetique
-	public function getInstruments(){
-		$req = 'SELECT nomInstrument FROM instruments ORDER BY nomInstrument' ;
-		if ($result = $this->_mysqli -> query($req)) {
+	public function getInstruments()
+	{
+		$req = 'SELECT nomInstrument FROM instruments ORDER BY nomInstrument';
+		if ($result = $this->_mysqli->query($req)) {
 			$dump  = $result->fetch_all(MYSQLI_ASSOC);
-			$result -> free_result();
+			$result->free_result();
 		}
 		return $dump;
 	}
 
 	// Récupère tableau avec les structures (et leurs données) 
-	public function getStructures(){
+	public function getStructures()
+	{
 		$req = 'SELECT * FROM structures';
-		if ($result = $this->_mysqli -> query($req)) {
-			  $dump = $result->fetch_all(MYSQLI_ASSOC);
+		if ($result = $this->_mysqli->query($req)) {
+			$dump = $result->fetch_all(MYSQLI_ASSOC);
 
-			  $result -> free_result();
-			}
-			return $dump;
+			$result->free_result();
+		}
+		return $dump;
 	}
 
 	// Récupère un tableau de Structure par une Rubrique
-	public function getStructureByRubrique($nomRubrique){
-		$req = "SELECT * FROM structures  INNER JOIN offres  ON structures.contact = offres.contact INNER JOIN rubriques ON rubriques.nomRubrique = offres.nomRubrique WHERE rubriques.nomRubrique = '$nomRubrique';  ";
-		if ($result = $this->_mysqli -> query($req)) {
-			  $dump = $result->fetch_all(MYSQLI_ASSOC);
-
-			  $result -> free_result();
-			}
-			return $dump;
+	public function getStructureByRubrique($nomRubrique)
+	{
+		$req = "SELECT * FROM structures  INNER JOIN offres  ON structures.contact = offres.contact INNER JOIN rubriques ON rubriques.nomRubrique = offres.nomRubrique WHERE rubriques.nomRubrique = '{$nomRubrique}';  ";
+		if ($result = $this->_mysqli->query($req)) {
+			$dump = $result->fetch_all(MYSQLI_ASSOC);
+			$dump = array_unique($dump);
+			$result->free_result();
+		}
+		return $dump;
 	}
 
+	// Récupère un tableau de Structure par une Rubrique
+	public function getStructureByInstrument($nomInstrument)
+	{
+		$req = "SELECT * FROM structures  INNER JOIN offres  ON structures.contact = offres.contact INNER JOIN instruments ON instruments.nomInstrument = offres.nomInstrument WHERE instruments.nomInstrument = '{$nomInstrument}';  ";
+		if ($result = $this->_mysqli->query($req)) {
+			$dump = $result->fetch_all(MYSQLI_ASSOC);
+			$dump = array_unique($dump);
+			$result->free_result();
+		}
+		return $dump;
+	}
 
 	// Verfie si l'utilisateur exite ( BOOL )
-	private function existsUser($user_mail){
+	private function existsUser($user_mail)
+	{
 		$exists = false;
 		$req = "SELECT mail from utilisateurs where mail='$user_mail'";
-		if ($result = $this->_mysqli -> query($req)) {
-			if( $result->num_rows != 0 ){
+		if ($result = $this->_mysqli->query($req)) {
+			if ($result->num_rows != 0) {
 				$exists = true;
-			}}
+			}
+		}
 		return $exists;
 	}
-	
+
 
 	//Methode pour verifier le login ( retourne un BOOL )
 	public function checkLogin($mail, $passwd)
 	{
 		$isLoginValid = false;
 		$req = "SELECT * from utilisateurs where mail='$mail' and motDePasse='$passwd'";
-		if ($result = $this->_mysqli -> query($req)) {
-			if( $result->num_rows != 0 ){
+		if ($result = $this->_mysqli->query($req)) {
+			if ($result->num_rows != 0) {
 				$isLoginValid = true;
 				// echo "<script>alert('Connexion reussie. ')</script>";
-			}else{
+			} else {
 				$user_check = $this->existsUser($mail);
-				if($user_check == false){
+				if ($user_check == false) {
 					echo "<script>alert('Adresse mail non reconnue. ')</script>";
-				}
-				else{
+				} else {
 					echo "<script>alert('Mot de passe incorrecte.')</script>";
 				}
 			}
@@ -113,14 +131,25 @@ class dbController{
 		return $isLoginValid;
 	}
 
-	public function cleanInput($input){
-		return ($this->_mysqli ->real_escape_string($input));
+	public function cleanInput($input)
+	{
+		return ($this->_mysqli->real_escape_string($input));
+	}
+
+	public function getOffresByRubrique($nomRubrique)
+	{
+		$req = "SELECT * FROM offres  WHERE nomRubrique = '{$nomRubrique}';  ";
+		if ($result = $this->_mysqli->query($req)) {
+			$dump = $result->fetch_all(MYSQLI_ASSOC);
+			$dump = array_unique($dump);
+			$result->free_result();
+		}
+		return $dump;
 	}
 
 
-	public function __destruct(){
+	public function __destruct()
+	{
 		$this->_mysqli->close();
 	}
-	
 }
-?>
